d5625b804454b1ef71dbe54089cbf2ab7557bb29
batshevavwebit <bat-sheva.v@webit-sys.com>
2017-11-27 13:35:21 +0200

    שינויים

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -12,6 +12,12 @@ import { Component, OnInit } from '@angular/core';
 
   <div class="col-md-6 bg-gray">
   <div class="container-lia container">
+  <div class="col-md-8 col col-md-offset-2">
+  <button class="btn btn-default btn-width bg-light-blue">הוסף / שנה חבילה </button>
+  <button class="btn btn-default btn-width bg-pink">הוסף מוצר </button>
+  <button class="btn btn-default btn-width bg-green">המשך תשלום לקוח קיים </button>
+  <button class="btn btn-default btn-width bg-blue">המשך לתשלום</button>
+
   </div>
   </div>
 
