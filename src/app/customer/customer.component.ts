9622a57387816ed4f25468c07c7182888cb6ba79
ayala bloy <ayalabloy12@gmail.com>
2017-11-28 16:52:20 +0200

    קקקקקקקקקקקקקקקקקקקקקקקקקקקקקקקקק

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -11,16 +11,23 @@ import { LiaService } from '../lia.service';
 <div class="t-center"><i class="icon-arrow-up-05 t-center green f-size-icon"></i></div>
 
 <div class="border-bottam-table green" *ngFor="let p of this.service.customers">
-<span class="col-md-3 col-xs-3">{{p.name}}</span>
-<span class="col-md-4 col-xs-4">{{p.address}}</span>
-<span class="col-md-4 col-xs-4">{{p.num}}</span>
-<span class="col-md-1 col-xs-1"><i class="icon-arrow-left-03 green f-size-icon"></i></span>
+    <div (click)="customerClicked()">
+        <span class="col-md-3 col-xs-3">{{p.name}}</span>
+        <span class="col-md-4 col-xs-4">{{p.address}}</span>
+        <span class="col-md-4 col-xs-4">{{p.num}}</span>
+        <span class="col-md-1 col-xs-1"><i class="icon-arrow-left-03 green f-size-icon"></i></span>
+    </div>
 </div>
-
+<div *ngIf="this.allowDetails">
+<p>uuuuuuuuu</p>
+</div>
 <div class="t-center"><i class="icon-arrow-down-04 t-center green f-size-icon"></i></div>
 </div>
 
 <div class="col-md-6 col-xs-6">
+<div>
+<input placeholder="חיפוש לקוח קיים"/>
+</div>
 <button routerLink="../cart">{{this.valueButton}}</button>
 </div>
 
@@ -34,12 +41,17 @@ import { LiaService } from '../lia.service';
   styles: []
 })
 export class CustomerComponent implements OnInit {
-valueButton:string;
+  valueButton:string;
+  allowDetails:boolean;
   constructor(private service:LiaService) { 
     this.valueButton="לקוח חדש";
     this.service.nowComponent="לקוחות";
+    this.allowDetails=false;
   }
-
+  customerClicked(){
+    this.allowDetails=!this.allowDetails;
+    this.valueButton=this.valueButton==="לקוח חדש"?"עבור לסל":"לקוח חדש";
+    }
   ngOnInit() {
   }
 
