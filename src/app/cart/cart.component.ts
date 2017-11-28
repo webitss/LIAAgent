f7f8ba1435eb628474ef093b9ff0691be33638a2
batshevavwebit <bat-sheva.v@webit-sys.com>
2017-11-28 10:48:31 +0200

    שינויים

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -9,16 +9,26 @@ import { serializePath } from '@angular/router/src/url_tree';
   <div>
   <div class="col-md-6">
   <div class="container-lia container">
+  <div class="t-center"><i class="icon-arrow-up-05 t-center blue f-size-icon"></i></div>
+  
+  <div class="border-bottam-table blue">
+  <span class="col-md-3 col-xs-3">שם העסק</span>
+  <span class="col-md-4 col-xs-4">כתובת</span>
+  <span class="col-md-4 col-xs-4">חבילת כרטסים שברשותו</span>
+  <span class="col-md-1 col-xs-1"><i class="icon-arrow-left-03 blue f-size-icon"></i></span>
+  </div>
+  
+  <div class="t-center"><i class="icon-arrow-down-04 t-center blue f-size-icon"></i></div>
   </div>
   </div>
 
   <div class="col-md-6 bg-gray">
   <div class="container-lia container">
   <div class="col-md-8 col col-md-offset-2">
-  <button class="btn btn-default btn-width bg-light-blue">הוסף / שנה חבילה </button>
-  <button class="btn btn-default btn-width bg-pink">הוסף מוצר </button>
-  <button class="btn btn-default btn-width bg-green">המשך תשלום לקוח קיים </button>
-  <button class="btn btn-default btn-width bg-blue">המשך לתשלום</button>
+  <button class="btn btn-width bg-light-blue">הוסף / שנה חבילה </button>
+  <button class="btn btn-width bg-pink">הוסף מוצר </button>
+  <button class="btn btn-width bg-green">המשך תשלום לקוח קיים </button>
+  <button class="btn btn-width bg-blue">המשך לתשלום</button>
 
   </div>
   </div>
